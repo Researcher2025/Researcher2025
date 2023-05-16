5fb95f97632b4b22f2efafccef310088571c1208
Suryaprakash Pandey <sppandey299@gmail.com>
2023-05-16 20:07:44 +0530

    Fixed Database

--- app.js
+++ app.js
@@ -1,23 +1,105 @@
-// Get all navbar links
-const navbarLinks = document.querySelectorAll("nav ul li a");
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const alert = require("alert");
+ 
+const app = express();
+ 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+ 
+mongoose.connect("mongodb://127.0.0.1:27017/userDB",{useNewUrlParser: true});
+ 
+const userSchema = {
+    email: String,
+    password: String
+};
+ 
+const User = new mongoose.model("User", userSchema);
+ 
+ 
+app.get("/", function (req, res) {
+  res.render('home');
+});
+ 
+app.get("/login", function(req, res) {
+  res.render("login");
+});
 
-// Add click event listener to each link
-navbarLinks.forEach(link => {
-  link.addEventListener("click", e => {
-    // Prevent default behavior of link
-    e.preventDefault();
+app.get("/addNews", function(req, res) {
+  res.render("addNews");
+});
+ 
+app.get("/register", (req, res)=>{
+  res.render("register");
+});
 
-    // Get section to scroll to
-    const sectionId = link.getAttribute("href");
-    const section = document.querySelector(sectionId);
+app.get("/newsandupdates", (req,res)=> {
+  res.render("newsandupdates");
+})
 
-    // Calculate position of section
-    const sectionTop = section.offsetTop;
+app.get("/addEvent", (req,res)=> {
+  res.render("addEvent");
+})
 
-    // Scroll to section
-    window.scrollTo({
-      top: sectionTop,
-      behavior: "smooth"
+app.get("/addjob", (req,res)=> {
+  res.render("addjob");
+})
+
+app.get("/careerresources", (req,res)=> {
+  res.render("careerresources");
+})
+
+app.get("/events", (req,res)=> {
+  res.render("events");
+})
+
+app.get("/notablealumni", (req,res)=> {
+  res.render("notablealumni");
+})
+ 
+app.post("/register", (req,res)=>{
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
     });
-  });
+ 
+    newUser.save()
+    .then(function(){
+        res.render("adminpage");
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+});
+ 
+app.post("/login", function(req,res){
+    const username = req.body.email;
+    const password = req.body.password;
+ 
+    User.findOne({email: username})
+    .then(function(foundUser){
+        if(foundUser.email==="admin@mmcoe.edu.in" && foundUser.password === password){
+            res.render("adminpage")
+        }
+        else if(foundUser.password === password) {
+          res.render("alumnipage")
+        }
+    })
+    .catch(function(err){
+        alert("Invalid details")
+        res.redirect("/login")
+        
+    })
+ 
+})
+ 
+app.listen(1000, function () {
+  console.log("Server started at port 1000");
 });
