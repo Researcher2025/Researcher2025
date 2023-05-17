57014d59af0fccd1ab52968573c48698e52b10f6
Suryaprakash Pandey <sppandey299@gmail.com>
2023-05-18 02:09:07 +0530

    Improved Backend

--- app.js
+++ app.js
@@ -5,36 +5,141 @@ const mongoose = require("mongoose");
 const alert = require("alert");
  
 const app = express();
- 
+
+// Set up the Express app
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
  
-mongoose.connect("mongodb://127.0.0.1:27017/userDB",{useNewUrlParser: true});
- 
+
+mongoose.connect('mongodb://127.0.0.1:27017/userDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB');
+});
+
 const userSchema = {
     email: String,
     password: String
 };
- 
 const User = new mongoose.model("User", userSchema);
- 
- 
-app.get("/", function (req, res) {
-  res.render('home');
+
+// Define the news schema and model
+const newsSchema = new mongoose.Schema({
+  subject: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  }
 });
+const News = mongoose.model('News', newsSchema);
+
+const eventsSchema = new mongoose.Schema({
+  eventname: {
+    type: String,
+    required: true
+  },
+  eventdate: {
+    type: Date,
+    required: true
+  },
+  eventlocation: {
+    type: String,
+    required: true
+  },
+  eventdescription: {
+    type: String,
+    required: true
+  }
+});
+const Events = mongoose.model('Events', eventsSchema);
+
+// Render the home page
+app.get('/', (req, res) => {
+  News.find().sort('-date').exec((err, news) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('An error occurred');
+    } else {
+      res.render('home', { news });
+    }
+  });
+});
+
+// Render the admin portal
+app.get('/addNews', (req, res) => {
+  res.render('addNews');
+});
+
+// Handle the submission of news data
+app.post('/addNews', (req, res) => {
+  const { subject, description, date } = req.body;
+
+  if (!subject || !description || !date) {
+    return res.status(400).send('Subject, description, and date are required');
+  }
+
+  const news = new News({
+    subject,
+    description,
+    date
+  });
+
+  news.save((err, news) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('An error occurred');
+    } else {
+      res.send('News added successfully');
+    }
+  });
+});
+
+app.post('/addEvent', (req, res) => {
+  const { eventname, eventdate, eventlocation, eventdescription } = req.body;
+
+  if (!eventname || !eventdate || !eventlocation || !eventdescription) {
+    return res.status(400).send('All fields are required');
+  }
+
+  const events = new Events({
+    eventname,
+    eventdate,
+    eventlocation,
+    eventdescription
+  });
+
+  events.save((err, events) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('An error occurred');
+    } else {
+      res.send('Event added successfully');
+    }
+  });
+});
+ 
+ 
+// app.get("/", function (req, res) {
+//   res.render('home');
+// });
  
 app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.get("/addNews", function(req, res) {
-  res.render("addNews");
-});
  
 app.get("/register", (req, res)=>{
   res.render("register");
